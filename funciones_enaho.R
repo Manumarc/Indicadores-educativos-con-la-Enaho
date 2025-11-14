450ed11262eeca680622c12fa935bb6561217426
Manuel Marcos <151560112+Manumarc@users.noreply.github.com>
2025-11-13 23:41:01 -0500

    Update funciones_enaho.R

--- funciones_enaho.R
+++ funciones_enaho.R
@@ -156,16 +156,7 @@ descargar_bases <- function(nom_encuesta, num_años, nom_modulos) {
     # Descomprimir el archivo .zip #
     #------------------------------#
     
-    #unzip(file_out, exdir = "01 Bases")
-    
-    # Función para descomprimir con windows
-    unzip_win <- function(zipfile, exdir){
-      
-      system2("unzip", args = c("-O", "cp850", zipfile, "-d", exdir))
-      
-    }
-
-    unzip_win(file_out, "01 Bases")
+    zip::unzip(file_out, exdir = "01 Bases")    
     
     # Registrar carpetas DESPUÉS de descomprimir #
     #--------------------------------------------#
